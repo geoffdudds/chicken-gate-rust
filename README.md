e485e40cdc36d1cacb343bb8855f4037992bfa8c
geoff.dudds <geoff.dudds@gmail.com>
2024-09-28 08:16:07 +0000

    builds - not tested

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ opens and closes my chicken gate
 
 # links
 - https://dev.to/mtnmts/containerized-builds-for-rust-on-the-esp32-e8m
+- https://dev.to/theembeddedrustacean/embassy-on-esp-getting-started-27fi
 - https://docs.esp-rs.org/book/writing-your-own-application/generate-project/index.html
 - https://github.com/esp-rs/esp-web-flash-server
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,68 @@
 #![no_std]
 #![no_main]
+#![feature(type_alias_impl_trait)]
 
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
-use esp_hal::{
-    clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*, system::SystemControl,
-};
+use esp_hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, system::SystemControl};
+// use esp_hal_embassy;
+// use esp_println::print;
 
-#[entry]
-fn main() -> ! {
-    let peripherals = Peripherals::take();
-    let system = SystemControl::new(peripherals.SYSTEM);
+// #[entry]
+// fn main() -> ! {
+//     let peripherals = Peripherals::take();
+//     let system = SystemControl::new(peripherals.SYSTEM);
 
-    let clocks = ClockControl::max(system.clock_control).freeze();
-    let delay = Delay::new(&clocks);
+//     let clocks = ClockControl::max(system.clock_control).freeze();
+//     let delay = Delay::new(&clocks);
 
-    esp_println::logger::init_logger_from_env();
+//     esp_println::logger::init_logger_from_env();
 
-    let timg0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0, &clocks);
-    let _init = esp_wifi::initialize(
-        esp_wifi::EspWifiInitFor::Wifi,
-        timg0.timer0,
-        esp_hal::rng::Rng::new(peripherals.RNG),
-        peripherals.RADIO_CLK,
-        &clocks,
-    )
-    .unwrap();
+//     let timg0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0, &clocks);
+//     let _init = esp_wifi::initialize(
+//         esp_wifi::EspWifiInitFor::Wifi,
+//         timg0.timer0,
+//         esp_hal::rng::Rng::new(peripherals.RNG),
+//         peripherals.RADIO_CLK,
+//         &clocks,
+//     )
+//     .unwrap();
+
+//     loop {
+//         log::info!("Hello world!");
+//         delay.delay(500.millis());
+//     }
+// }
+
+#[embassy_executor::task]
+async fn one_second_task() {
+    let mut count = 0;
 
     loop {
-        log::info!("Hello world!");
-        delay.delay(500.millis());
+        esp_println::println!("Spawn Task Count: {}", count);
+        count += 1;
+        Timer::after(Duration::from_millis(1_000)).await;
+    }
+}
+
+#[main]
+async fn main(spawner: Spawner) {
+    let peripherals = Peripherals::take();
+    let system = SystemControl::new(peripherals.SYSTEM);
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    esp_hal_embassy::init(
+        &clocks,
+        esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0, &clocks).timer0,
+    );
+
+    spawner.spawn(one_second_task()).unwrap();
+
+    let mut count = 0;
+    loop {
+        esp_println::println!("Main Task Count: {}", count);
+        count += 1;
+        Timer::after(Duration::from_millis(5_000)).await;
     }
 }
